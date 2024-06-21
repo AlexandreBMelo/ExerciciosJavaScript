6efe50211fa4a4b0e2ff6afc35237355bb247466
AlexandreMelo <alemelo18lds@gmail.com>
2024-06-21 10:05:10 -0300

    Commit 02

---
+++ ContagemProgressiva.js
@@ -0,0 +1,6 @@
+let numero = prompt("Insira um numero: ");
+let numeroInicial = 0;
+while (numeroInicial <= numero){
+    console.log(numeroInicial);
+    numeroInicial++;
+}
---
+++ Contagemregressiva.js
@@ -0,0 +1,6 @@
+let numero = prompt("Insira um numero: ");
+
+while (numero >= 0){
+    console.log(numero);
+    numero--;
+}
